073e439a4442fbefd912e1c1ba4ad825b2998d7a
Antoine <67126866+ChiefsBestPal@users.noreply.github.com>
2023-02-17 00:47:10 -0500

    Backend: add API server future app folder run

---
+++ backend/application/__init__.py
@@ -0,0 +1,6 @@
+#TODO here: __all__ = ["main"], instantiate app
+#then in the highest level dir of backend, add a run.py to simply
+#catch the __name__ == __main__ scope and run the create_app() result
+#with run() and init_db if needed or 'with context man' app_context()
+#on the appropriate host and port for the server launched
+#for developement at least
