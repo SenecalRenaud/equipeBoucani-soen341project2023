50c87a9d3c0106a64ab88c00381bb9f3faa0064a
Antoine <67126866+ChiefsBestPal@users.noreply.github.com>
2023-02-21 18:30:14 -0500

    Moved+Integrated all models & schemas to model.py
    To allow a clearer view of the current simple Flask API I made, I officially transfered the "test" model and schema for the table "CommentPost", 
    as well as the SQLAlchemy db object and the Marshmallow schema interface object. They will have to be init'ed from the running module and if models need to be created one will need to use either app_context()'s scope or flask's local running app. (Check app.py my newest commit to app.py)

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,37 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
+import datetime
 
 from uuid import uuid1,uuid4,uuid5
-#db.init_app(app) in __main__ if db=SQLAlchemy() created here !
-def generate_pr_uuid():
-    return uuid4().hex
+generate_pr_uuid = lambda : uuid4().hex
+
+db = SQLAlchemy()
+
+ma = Marshmallow()
+class CommentPost(db.Model):
+    """
+    Experiment MySQL Alchemy Table creation and config with OOP.
+    Python interpreter simplest launch example
+    >>> from app import db
+    >>> db.create_all()
+    """
+    __slots__ = ()
+    __tablename__ = "comment_post"
+
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(100),unique=True,nullable=False)
+    body = db.Column(db.Text())
+    date = db.Column(db.DateTime,default=datetime.datetime.now)
+
+    def __init__(self,title,body):
+        self.title = title
+        self.body = body
+
+class CommentPostSchema(ma.Schema):
+    class Meta:
+        fields = ('id','title','body','date')
+
+    # _links = ma.Hyperlinks(
+
 
