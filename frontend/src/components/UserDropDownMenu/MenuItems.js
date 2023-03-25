dd7fd15f9e3002e22f2e1ae5e931be5d6c09821f
Antoine Cantin <antoine.crafter123@gmail.com>
2023-03-25 15:58:19 -0400

    Requirements fixes and fixed some issues with MenuItems FSM

--- frontend/src/components/UserDropDownMenu/MenuItems.js
+++ frontend/src/components/UserDropDownMenu/MenuItems.js
@@ -34,55 +34,54 @@ export default function UserRelationPermsFSM(user1,user2){
   switch (isSelf | ub2 | ub1) {
 
     case 0b00101: //Applicant views other Applicant
-      return <></>
+      return <div></div>
     case 0b10101: //Applicant views itself
-        console.log("AAAAAAAA")
       return <div>
         <li> <Link to="/current/applications"> My Applications </Link> </li>
         <li> <Link to="/current/interviews"> My Interviews </Link> </li>
     </div>
     case 0b01001: //Applicant views Employer
         //TODO : Might want to encrypt uid here if could be dangerous to leak it / or risk it being hacked
-      return <>
+      return <div>
         <li> <Link to={"/" + user2.uid +  "/jobpostings"}>
           {user2.firstName} {user2.lastName}'s Job Postings </Link> </li>
-      </>
+      </div>
     case 0b01101: //Applicant views Admin
-      return <></>
+      return <div></div>
     case 0b00110: //Employer views Applicant
-      return <>
+      return <div>
       <li>
         <Link to={"/" + user2.uid + "/makeOffer"}> Make {user2.firstName} {user2.lastName} an offer</Link>
       </li>
-      </>
+      </div>
     case 0b01010: //Employer views other Employer
-      return <>
+      return <div>
       <li> <Link to={"/" + user2.uid +  "/jobpostings"}>
           {user2.firstName} {user2.lastName}'s Job Postings </Link> </li>
-      </>
+      </div>
     case 0b11010: //Employer views itself
-      return <>
+      return <div>
         <li> <Link to="/current/jobpostings"> My Job Postings </Link></li>
         <li> <Link to="/current/interviews"> My Interviews </Link></li>
-      </>
+      </div>
     case 0b01110: //Employer views Admin
-      return <></>
+      return <div></div>
     case 0b00111: //Admin views Applicant
-      return <>
+      return <div>
         <li> <Link to={"/" + user2.uid +  "/applications"}> {user2.email}'s Applications </Link> </li>
         <li> <Link to={"/" + user2.uid +  "/interviews"}> {user2.email}'s Interviews </Link> </li>
-      </>
+      </div>
     case 0b01011: //Admin views Employer
-      return <>
+      return <div>
         <li> <Link to={"/" + user2.uid +  "/jobpostings"}> {user2.email}'s Job Postings </Link> </li>
         <li> <Link to={"/" + user2.uid +  "/interviews"}> {user2.email}'s Interviews </Link> </li>
-      </>
+      </div>
     case 0b01111: //Admin views other Admin
-      return <></>
+      return <div></div>
     case 0b11111: //Admin views itself
-      return <>
+      return <div>
       <li> <Link to={"/admin_commandpanel"}> Admin Command Panel </Link></li>
-      </>
+      </div>
     default:
       throw new Error("Unknown bitflag: Interaction not stated.")
 
