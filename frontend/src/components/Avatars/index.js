3282ad5d1eaa7605be81064e53bcbfe19f21e837
Antoine Cantin <antoine.crafter123@gmail.com>
2023-03-27 21:41:02 -0400

    Made reusable custom user Avatars with Material UI Design core

---
+++ frontend/src/components/Avatars/index.js
@@ -0,0 +1,85 @@
+import React from "react";
+import styled, { ThemeProvider } from "styled-components";
+import Typography from "@material-ui/core/Typography";
+import Avatar from "@material-ui/core/Avatar";
+import {AvatarGroup} from "@material-ui/lab";
+import { StylesProvider, useTheme } from "@material-ui/core/styles";
+
+const AvatarContainer = styled.div`
+  display: flex;
+  margin-bottom: 14px;
+  & > * {
+    margin: 4px;
+  }
+
+`;
+
+// const SizedAvatar = styled(Avatar)`
+//   ${({ size, theme }) => `
+//     width: ${theme.spacing(size)}px;
+//     height: ${theme.spacing(size)}px;
+//   `};
+// `;
+
+const AvatarLabel = styled.div`
+  display: flex;
+  align-items: center;
+    &:hover {
+      cursor: pointer;
+      div {
+        border: 3px solid lightseagreen;
+      }
+      * {
+        color: lightseagreen
+      }
+      
+      
+  }
+`;
+
+const BorderedAvatar = styled(Avatar)`
+  border: 3px solid lightseagreen;
+`;
+
+function UserAvatarWithText(userObj,key=0,avatarSize=6) {
+
+
+  return (
+
+    <AvatarContainer>
+      <AvatarLabel>
+        <Avatar
+            key={key}
+          style={{ marginRight: "14px"}}
+          // size={avatarSize}
+          alt={userObj.firstName + " " + userObj.lastName}
+          src={userObj.photo_url}
+        />
+        <Typography variant="body2"> {userObj.firstName + " " + userObj.lastName}</Typography>
+      </AvatarLabel>
+    </AvatarContainer>
+
+  );
+}
+function UserAvatarGroup(usersList,maxNumOfUsersShown) {
+  return (
+    <AvatarContainer>
+      <AvatarGroup max={maxNumOfUsersShown}>
+          {
+              usersList.map((userObj,ix) =>
+                  (
+                      <Avatar
+                        key={ix}
+                        alt={userObj.firstName}
+                        src={userObj.photo_url}
+                        />
+                  )
+              )
+          }
+
+
+      </AvatarGroup>
+    </AvatarContainer>
+  );
+}
+export {UserAvatarWithText,UserAvatarGroup }
