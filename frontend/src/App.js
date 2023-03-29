09bbd56985ad127b8af5ffd6334373f17099e1bd
Antoine Cantin <antoine.crafter123@gmail.com>
2023-03-29 02:40:27 -0400

    [Major] Attacks and Errors handling on logged in user... asynchronously checking in App.js (Strict mode: 2x per load). Upon manual changes to cookies or cache, users are forcefully logged out in various scenarios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import './App.css';
 
 import { Header, Navbar} from './components';
@@ -16,6 +16,7 @@ import SignIn from "./pages/SignIn/SignInForm";
 import ViewJobPosts from "./pages/JobPosts/ViewJobPosts";
 import CommentAPIService from "./pages/BACKEND_DEBUG/CommentAPIService";
 import Cookies from 'js-cookie';
+import UserRESTAPI from "./restAPI/UserAPI";
 
 function App() {
     const [comments, setComments] = useState([""]);
@@ -28,15 +29,68 @@ function App() {
         let new_jobs = [...jobs, job]
         setJobs(new_jobs)
     }
+
+    useEffect(
+        () => {
+                (async function assertFrontendAuthIsSyncedWithBackendSession() {
+        /*
+        Antoine@ChiefsBestPal
+        do not change without review,
+        even if possibly subject to necessary changes in near future.
+         */
+      const userLoggedInBackendSession = await UserRESTAPI.fetchCurrentUserLoggedInBackendSession();
+      // UserRESTAPI.userLoggedInBackendSession = userLoggedInBackendSession;
+      //    console.log(userLoggedInBackendSession)
+
+      if (Object.keys(userLoggedInBackendSession).length === 0
+      && !UserRESTAPI.checkIfAllUserFrontendCacheAvailable()
+      && !UserRESTAPI.checkIfAllUserCookiesAvailable()){ // No user logged in backend session
+            // console.log("CCCCCCCCCCCCC")
+          CommentAPIService.UserLogout({frontend_logout_only : true}) //Clean up frontend auth only
+
+          return;
+      }
+      if (!UserRESTAPI.checkIfAllUserCookiesAvailable()) { // Some authentification / token cookies are missing, frontend unauthorized
+          // console.log("BBBBBBBB")
+          alert("BOUCANI SECURITY: Some User Auth / Token Cookies manual alterations were detected. \n\r\tImmediate logout. \n\r\t Status: 401")
+          CommentAPIService.UserLogout() // Completely logout server and client side
+          window.location.replace('http://localhost:3000')
+          return;
+      }
+      //From this point:
+          // Authorized user logged in Backend
+          // User Cookies are valid
+      let userLoggedInFrontendAuth;
+      try {
+          userLoggedInFrontendAuth = UserRESTAPI.parseCurrentUserObjFromFrontendCache()
+          if(Object.keys(userLoggedInFrontendAuth).length === 0)
+              throw new Error("Incomplete (or empty) frontend session cache for logged in user descriptor")
+      } catch( frontendCachedUserIncompleteError ) {
+          // console.log("AAAAAAAAAAAA")
+          alert("BOUCANI SECURITY: Some Local User Descriptor / Cache  manual alterations were detected. \n\r\tImmediate logout. \n\r\t Status: 401")
+          CommentAPIService.UserLogout() // Completely logout server and client side
+          window.location.replace('http://localhost:3000')
+          return;
+
+      }
+        // console.log("DDDDDDDDDDDDD")
+      if (Object.keys(userLoggedInBackendSession).length !== 0){
+            await UserRESTAPI.updateCachedFrontendUserFromBackendSession(
+            {userObj: userLoggedInBackendSession}
+      )
+      }
+
+    })()
+        }
+        ,[]
+    )
+
 return (
     <Router>
     <Navbar />
-        {
-            Cookies.get("BACKEND_SESSION_ENDED") != null &&
-                CommentAPIService.UserLogout()
 
-        }
         {console.log("Loaded App.js")}
+
     <Routes>
         <Route exact path='/'  element={<Home />} />
         <Route path='/jobposting' element={<JobPostingForm/>} />
--- frontend/src/restAPI/UserAPI.js
+++ frontend/src/restAPI/UserAPI.js
@@ -1,6 +1,7 @@
 import CommentAPIService from "../pages/BACKEND_DEBUG/CommentAPIService";
+import Cookies from "js-cookie";
 
-    const USER_FIELDS = [
+export const USER_FIELDS = [
     "firstName",
     "lastName",
     "email",
@@ -12,24 +13,42 @@ import CommentAPIService from "../pages/BACKEND_DEBUG/CommentAPIService";
     "uid"
 ]
 
+export const USER_COOKIES = [
+    "access_token",
+    "refresh_token",
+    "loggedin_uid"
+]
+
 export default class UserRESTAPI  {
 
     //TODO: MOVE SOME THINGS FROM COMMENT POST API TO HERE
     //get UserNameSpace
-    static parseCurrentUserObjFromCookies() {
+    static parseCurrentUserObjFromFrontendCache() {
         //TODO UUID ENCRYPTION SECURITY
         var userObj = {},
-        keys = Object.keys(localStorage),
+        keys = USER_FIELDS,//Object.keys(localStorage),
         i = keys.length;
 
         while ( i-- ) {
             userObj[keys[i]] = localStorage.getItem(keys[i]) ;
+            if (userObj[keys[i]] === "undefined" || userObj[keys[i]] == null) {
+                return {}
+            }
         }
         return userObj;
     }
+    static getAllUserCookies() {
+        var cookiesObj = {},
+        keys = USER_COOKIES,//Object.keys(localStorage),
+        i = keys.length;
+
+        while ( i-- ) {
+            cookiesObj[keys[i]] = Cookies.get(keys[i]) ;
+        }
+        return cookiesObj;
+    }
     //get UserNameSpace
     static async parseUserFromUidBackendRequest(uid,callback) {
-        //TODO To be improved with UserRESTAPI
 
         return await CommentAPIService.GetUserDetails(uid)
             .then(
@@ -38,7 +57,72 @@ export default class UserRESTAPI  {
     }
     static checkIfObjectIsValidUser(obj) {
         for (let property of USER_FIELDS){
-            if (!obj.hasOwnProperty(property)){
+            if (!obj.hasOwnProperty(property) ||
+                obj[property] === undefined ||
+                obj[property] === 'undefined'
+            ){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static userLoggedInBackendSession;
+
+
+    static updateCachedFrontendUserFromBackendSession({userObj = null}){
+
+        if (UserRESTAPI.userLoggedInBackendSession === undefined)
+            UserRESTAPI.fetchCurrentUserLoggedInBackendSession();
+
+        let _userLoggedInBackendSession = (userObj == null)
+            ? UserRESTAPI.userLoggedInBackendSession : userObj
+
+        for (let property of USER_FIELDS){
+            window.localStorage.setItem(property,
+                _userLoggedInBackendSession[property]
+            )
+        }
+
+    }
+    static async isUserLoggedInBackendSession(){
+        try {
+              const result = await UserRESTAPI.fetchCurrentUserLoggedInBackendSession();
+              return {userLoggedInBackendSession : result ,
+                       isLoggedInSession:  Object.keys(result).length !== 0};
+            } catch (error) {
+              console.error(error);
+        }
+    }
+    static async fetchCurrentUserLoggedInBackendSession(){
+        // UserRESTAPI.res = Object.keys(json).length !== 0
+            try {
+              const result = await CommentAPIService.GetUserDetails("current");
+              UserRESTAPI.userLoggedInBackendSession = result;
+              return result;
+            } catch (error) {
+              console.error(error);
+        }
+    }
+    static checkIfAllUserFrontendCacheAvailable() {
+        var keys = USER_FIELDS,
+        i = keys.length;
+
+        while ( i-- ) {
+            if (localStorage.getItem(keys[i]) == null ||
+                localStorage.getItem(keys[i]) === undefined){
+                return false;
+            }
+        }
+        return true;
+    }
+    static checkIfAllUserCookiesAvailable() {
+        var keys = USER_COOKIES,
+        i = keys.length;
+
+        while ( i-- ) {
+            if (Cookies.get(keys[i]) == null ||
+                Cookies.get(keys[i]) === undefined){
                 return false;
             }
         }
