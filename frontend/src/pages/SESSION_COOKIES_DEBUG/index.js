c49ef514d2870f0047c4c4b5c7e6f879f97b755d
ChiefsBestPal <67126866+ChiefsBestPal@users.noreply.github.com>
2023-04-01 01:09:31 -0400

    Cleanup old contexts and session cookie debug page

--- frontend/src/pages/SESSION_COOKIES_DEBUG/index.js
+++ frontend/src/pages/SESSION_COOKIES_DEBUG/index.js
@@ -1,130 +1,131 @@
-import React, {useEffect, useState} from 'react';
-import Cookies from 'js-cookie';
-import CommentAPIService from "../BACKEND_DEBUG/CommentAPIService";
-import {UserAvatarWithText} from "../../components/Avatars";
-import UserDropDownMenu from "../../components/UserDropDownMenu/UserDropDownMenu";
-
-const SESSION_COOKIES_DEBUG  = () => {
-        function getAllCookies() {
-      return fetch("http://localhost:5000/getall-cookies/", {
-        credentials: "include"
-      }).then(response => {
-        return response.json()
-      }).then(json => {
-            var htmluserinfo = document.getElementById("loggedinUserInfo")
-            htmluserinfo.innerText = JSON.stringify(json,null,'\t')
-      });
-    }
-
-    function getACookie() {
-      return fetch("http://localhost:5000/get-cookie/", {
-        credentials: "include"
-      }).then(response => {
-        // make sure to check response.ok in the real world!
-        return Promise.resolve("All good, fetch the data");
-      });
-    }
-
-
-    function getData() {
-      fetch("http://localhost:5000/api/cookies_test/", {
-        credentials: "include"
-      })
-        .then(response => {
-          // make sure to check response.ok in the real world!
-          return response.json();
-        })
-        .then(json =>
-        {
-            console.log(json);
-            // TODO HERE: SET OR UNSET CLIENT-SIDE COOKIES
-            // setLoggedinUser(json);
-            var htmluserinfo = document.getElementById("loggedinUserInfo")
-            htmluserinfo.innerText = JSON.stringify(json,null,'\t')
-        });
-    }
-    const [user,setUser] = useState([{}]);
-    const fetchUserData = () => {
-            //document.forms[0].elements["inputUserId"].value
-            return CommentAPIService.GetUserDetails(
-                document.getElementById("inputUserId").value
-            ).then(
-                (data) => {
-                    console.log(data)
-                    setUser(data)
-                }
-            )
-    }
-
-    return (
-        <div>
-            <p style={{margin: "auto 5rem", color: "white"}}> We want cross origin backend to be able to give his
-                user session cookies to frontend, a frontend that
-                is able to AJAX request them normally and gain access
-                to required headers and application/* binaries as to
-                make a client-side API that allow frontend to know
-                user info and log in state... locally... without
-                creating vulnerabilities with localStorage since everything
-                dangerous would still be in backend
-            </p>
-
-
-                <textarea name="inputUserId" id="inputUserId"></textarea>
-                <button onClick={fetchUserData}>Try to find user!</button>
-
-
-
-            <article id="displayedUserAvatar">
-                {
-
-                    UserAvatarWithText(user,0)
-                }
-                {
-                                        <UserDropDownMenu
-                    triggerMenuMarkup={UserAvatarWithText(user,0)}
-                    triggeredUserUid={user.uid}
-                    />
-                }
-            </article>
-
-
-            <button id="ajaxFetchCookiesButton"
-                    onClick=
-                        {(e)=>
-                        {getACookie().then(() => getData());}}
-                    >
-                FETCH
-            </button>
-            <p></p>
-            <button id="ajaxFetchCookiesButton"
-                    onClick=
-                        {(e)=>
-                        {getAllCookies()}}
-                    >
-                SHOW ALL COOKIES
-            </button>
-            <br/>
-            <br/>
-            <button id="removeCookies"
-                    onClick=
-                        {(e)=>
-                        {CommentAPIService.UserLogout();window.location.replace('http://localhost:3000')}}
-            >
-                SignOut
-            </button>
-            <section style={{color: "lightblue"}}>
-            <h3 > Logged In user info: {Cookies.get('loggedin_uid')}</h3>
-
-            <article id="loggedinUserInfo">
-
-            </article>
-                </section>
-
-
-        </div>
-    )
-
-
-}
-
-export default SESSION_COOKIES_DEBUG;
+// import React, {useEffect, useState} from 'react';
+// import Cookies from 'js-cookie';
+// import CommentAPIService from "../BACKEND_DEBUG/CommentAPIService";
+// import {UserAvatarWithText} from "../../components/Avatars";
+// import UserDropDownMenu from "../../components/UserDropDownMenu/UserDropDownMenu";
+//
+// const SESSION_COOKIES_DEBUG  = () => {
+//         function getAllCookies() {
+//       return fetch("http://localhost:5000/getall-cookies/", {
+//         credentials: "include"
+//       }).then(response => {
+//         return response.json()
+//       }).then(json => {
+//             var htmluserinfo = document.getElementById("loggedinUserInfo")
+//             htmluserinfo.innerText = JSON.stringify(json,null,'\t')
+//       });
+//     }
+//
+//     function getACookie() {
+//       return fetch("http://localhost:5000/get-cookie/", {
+//         credentials: "include"
+//       }).then(response => {
+//         // make sure to check response.ok in the real world!
+//         return Promise.resolve("All good, fetch the data");
+//       });
+//     }
+//
+//
+//     function getData() {
+//       fetch("http://localhost:5000/api/cookies_test/", {
+//         credentials: "include"
+//       })
+//         .then(response => {
+//           // make sure to check response.ok in the real world!
+//           return response.json();
+//         })
+//         .then(json =>
+//         {
+//             console.log(json);
+//             // TODO HERE: SET OR UNSET CLIENT-SIDE COOKIES
+//             // setLoggedinUser(json);
+//             var htmluserinfo = document.getElementById("loggedinUserInfo")
+//             htmluserinfo.innerText = JSON.stringify(json,null,'\t')
+//         });
+//     }
+//     const [user,setUser] = useState([{}]);
+//     const fetchUserData = () => {
+//             //document.forms[0].elements["inputUserId"].value
+//             return CommentAPIService.GetUserDetails(
+//                 document.getElementById("inputUserId").value
+//             ).then(
+//                 (data) => {
+//                     console.log(data)
+//                     setUser(data)
+//                 }
+//             )
+//     }
+//
+//     return (
+//         <div>
+//             <p style={{margin: "auto 5rem", color: "white"}}> We want cross origin backend to be able to give his
+//                 user session cookies to frontend, a frontend that
+//                 is able to AJAX request them normally and gain access
+//                 to required headers and application/* binaries as to
+//                 make a client-side API that allow frontend to know
+//                 user info and log in state... locally... without
+//                 creating vulnerabilities with localStorage since everything
+//                 dangerous would still be in backend
+//             </p>
+//
+//
+//                 <textarea name="inputUserId" id="inputUserId"></textarea>
+//                 <button onClick={fetchUserData}>Try to find user!</button>
+//
+//
+//
+//             <article id="displayedUserAvatar">
+//                 {
+//
+//                     UserAvatarWithText(user,0)
+//                 }
+//                 {
+//                                         <UserDropDownMenu
+//                     triggerMenuMarkup={UserAvatarWithText(user,0)}
+//                     triggeredUserUid={user.uid}
+//                     />
+//                 }
+//             </article>
+//
+//
+//             <button id="ajaxFetchCookiesButton"
+//                     onClick=
+//                         {(e)=>
+//                         {getACookie().then(() => getData());}}
+//                     >
+//                 FETCH
+//             </button>
+//             <p></p>
+//             <button id="ajaxFetchCookiesButton"
+//                     onClick=
+//                         {(e)=>
+//                         {getAllCookies()}}
+//                     >
+//                 SHOW ALL COOKIES
+//             </button>
+//             <br/>
+//             <br/>
+//             <button id="removeCookies"
+//                     onClick=
+//                         {(e)=>
+//                         {CommentAPIService.UserLogout();
+//                             window.location.replace('http://localhost:3000')}}
+//             >
+//                 SignOut
+//             </button>
+//             <section style={{color: "lightblue"}}>
+//             <h3 > Logged In user info: {Cookies.get('loggedin_uid')}</h3>
+//
+//             <article id="loggedinUserInfo">
+//
+//             </article>
+//                 </section>
+//
+//
+//         </div>
+//     )
+//
+//
+// }
+//
+// export default SESSION_COOKIES_DEBUG;
