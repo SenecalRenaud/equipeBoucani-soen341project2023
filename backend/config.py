11b5a302cd55f2a5d594e76802a62976995b79ad
Antoine Cantin <antoine.crafter123@gmail.com>
2023-04-03 18:07:20 -0400

    Metaclass EnumABC to handle obligatory database fields and store safely extra information in the same object: Tested and Working,MRO/hierarchy working

--- backend/config.py
+++ backend/config.py
@@ -1,9 +1,11 @@
+import json
+
 from dotenv import load_dotenv,find_dotenv
 #Avoid (dotenv.find_dotenv(".myenvfilename"))
 import os
 #import redis #do from_url(), >redis-cli to get localhost redis uri
 import enum
-from abc import ABC, ABCMeta,abstractmethod
+from abc import ABC, ABCMeta,abstractmethod,abstractclassmethod
 
 import dotenv
 
@@ -22,63 +24,91 @@ except:
                             f"should be in \033[1m./backend/\033[0m.\n\r"
                             f">>> Contact Antoine@ChiefsBestPal for necessary auth or other admins if problems")
 
-class MetaDatabaseConnection(enum.EnumMeta,ABCMeta):
+class ABCMetaDatabaseConnection(enum.EnumMeta, ABCMeta):
     """
-    Database Connection Server info Meta class
-    ChiefsBestPal@AntoineCantin
+    Database Connection Server info Meta x Abstract Mixin class
+    AntoineCantin@ChiefsBestPal
     """
     OBLIGATORY_NAMES = ['RDBMS_ALCHEMY_HNAME', 'DB_NAME', 'DB_USER', 'DB_PASS', 'DB_HOST', 'DB_PORT'] #not hashset, needs to be ordered
     def __new__(metacls, cls, bases, classdict):
 
-        classdict['_ignore_'] = 'OBLIGATORY_NAMES'
 
-        bases = tuple([MetaDatabaseConnection.CurrentDatabaseEnum] + list(bases)[1:])
-
-        object_attrs = set(dir(type(cls, (object,), {})))
+        print(metacls, cls, bases, classdict)
+        # bases = tuple([ABCMetaDatabaseConnection.CurrentDatabaseABCEnum] + list(bases)[1:])
 
         db_template_enum_obj = super().__new__(metacls, cls, bases, classdict)
 
-        
+        if db_template_enum_obj.__qualname__ == "CurrentDatabaseABCEnum":
+            return db_template_enum_obj
+
+        object_attrs = set(dir(type(cls, (object,), {})))
+
         db_template_enum_obj._member_names_ = set(classdict.keys()) - object_attrs
-
+        # print("MEMNAMES:", db_template_enum_obj._member_names_)
+        # print("ATTRS: ", set(classdict))
+        # non_defaulted_members =
+        classdict['_ignore_'] = "OBLIGATORY_NAMES"
         non_members = set()
+        if db_template_enum_obj._member_map_:
+            try:
+                if (absmethods := list(db_template_enum_obj.__abstractmethods__)):
+                    missing = ', '.join(f'{method!r}' for method in absmethods)
+                    raise TypeError(
+                       f"Cannot instantiate Abstract/MixinABC {db_template_enum_obj.__name__!r}:"
+                       f"Without implementing abstract methods: {missing}")
+            except AttributeError:
+                pass #Not ABC, skip
 
-        for attr in db_template_enum_obj._member_names_:
 
-            if attr.startswith('_') and attr.endswith('_'):
 
-                non_members.add(attr)
+        # print("MEMBERS: ",db_template_enum_obj.__members__)
+        # print("MAP: ", db_template_enum_obj._member_map_)
+        # print("NAMES: ", db_template_enum_obj._member_names_)
+        for attr_name in set(db_template_enum_obj._member_names_)-{'databaseServerURL','exportToNameSpace'}:
+
+            if attr_name.startswith('_') and attr_name.endswith('_'):
+
+                non_members.add(attr_name)
 
             else:
+                print(f"GETATTR ('{attr_name}') : {getattr(db_template_enum_obj,attr_name)}")
+                setattr(db_template_enum_obj, attr_name, attr_name)
 
-                setattr(db_template_enum_obj, attr, attr)
+        db_template_enum_obj._member_names_.difference_update(non_members | {'OBLIGATORY_NAMES'})
 
-        db_template_enum_obj._member_names_.difference_update(non_members | set(classdict.get('_ignore_',[])))
-
-        assert not (set(db_template_enum_obj.__members__) - set(MetaDatabaseConnection.OBLIGATORY_NAMES))
-
-
+        assert not (set(ABCMetaDatabaseConnection.OBLIGATORY_NAMES) - set(db_template_enum_obj.__members__.keys()))
+        # print(db_template_enum_obj._member_names_)
+        # print(json.dumps(dict(db_template_enum_obj.__members__.items()),indent=2))
         return db_template_enum_obj
 
-    class CurrentDatabaseEnum(enum.Enum,ABC):
-        # _order_ = ('RDBMS_ALCHEMY_HNAME', 'DB_NAME', 'DB_USER', 'DB_PASS', 'DB_HOST', 'DB_PORT')
 
-        @abstractmethod #@abstractproperty has been deprecated !
-        def databaseServerURL(self):
-            pass
-        @classmethod
-        def exportToNameSpace(cls,namespace : dict,onlyObligatoryFields=True):
-            members  = cls.__members__
-            if onlyObligatoryFields:
-                members = {key: value for key, value in members.items() if key in MetaDatabaseConnection.OBLIGATORY_NAMES}
-            namespace.update(members)
-            return tuple(map(lambda enum_field: enum_field.value,
-                             cls._member_map_.values()))
-class LocalHostDatabase(MetaDatabaseConnection.CurrentDatabaseEnum, metaclass=MetaDatabaseConnection):
+class CurrentDatabaseABCEnum(metaclass=ABCMetaDatabaseConnection):
+    """
+    Abstract Enum that holds no actual information but forces metaclass in MRO
+    and thus forces its Mixin inherihors to implement as specified here, as well as get any class or static methods,etc
+    AntoineCantin@ChiefsBestPal
+    """
+    # _ignore_ = ['databaseServerURL','exportToNameSpace']
+    # _order_ = ('RDBMS_ALCHEMY_HNAME', 'DB_NAME', 'DB_USER', 'DB_PASS', 'DB_HOST', 'DB_PORT')
+
+    @property
+    @abstractmethod #@abstractproperty has been deprecated !
+    def databaseServerURL(self):
+        pass
+    @classmethod
+    def exportToNameSpace(cls,namespace : dict,*,onlyObligatoryFields=True):
+        _members_map  = cls.__members__
+
+        if onlyObligatoryFields:
+            _members_map = {key: value for key, value in _members_map.items() if key in ABCMetaDatabaseConnection.OBLIGATORY_NAMES}
+        namespace.update(_members_map)
+        return tuple(map(lambda enum_field: enum_field.value,
+                         _members_map.values()))
+class LocalHostDatabase(CurrentDatabaseABCEnum, enum.Enum, metaclass=ABCMetaDatabaseConnection):
     """
     Current Test Database connection info...
     locally hosted tables using XAMMP and mysql+apache server
-    Antoine Cantin
+    AntoineCantin@ChiefsBestPal
     """
     RDBMS_ALCHEMY_HNAME = 'mysql'
     DB_NAME = "flask_test_mysql_db"
@@ -90,7 +120,7 @@ class LocalHostDatabase(MetaDatabaseConnection.CurrentDatabaseEnum, metaclass=Me
     @property
     def databaseServerURL(self):
         return rf"jdbc:mysql://localhost:3306/flask_test_mysql_db"
-class HostedSql96Database(MetaDatabaseConnection.CurrentDatabaseEnum, metaclass=MetaDatabaseConnection):
+class HostedSql96Database(CurrentDatabaseABCEnum, enum.Enum, metaclass=ABCMetaDatabaseConnection):
     """
     From freemysqlhosting.net account.
     Must have the right values in .env.local or any loaded .env file for databases
@@ -103,15 +133,15 @@ class HostedSql96Database(MetaDatabaseConnection.CurrentDatabaseEnum, metaclass=
     DB_HOST = "localhost"#TODO
     DB_PORT = ""#e.g. :5000, :3306#TODO
 
+    #NOTE These fields will be ignored except if exportToNameSpace(namespace,onlyObligatoryFields=False)
+    BASIC_MB_SPACE = 5
+    SERVER_LOCATION = "NA-East"
     @property
     def databaseServerURL(self):
         return r"jdbc:mysql://localhost:3306/flask_test_mysql_db"
 
 RDBMS_ALCHEMY_HNAME,DB_NAME,DB_USER,DB_PASS,DB_HOST,DB_PORT = \
     HostedSql96Database.exportToNameSpace(globals()) #NOTE: Current database is chosen here, and relays its members to module scope
-
-#TODO OFFER CHOICE BETWEEN LOCAL AND HOSTED DATABASE IN BACKEND CONSOLE !
-
 
 class ApplicationSessionConfig:
     SECRET_KEY = os.environ["SECRET_KEY"] # should set flask.Flask.secret_key
