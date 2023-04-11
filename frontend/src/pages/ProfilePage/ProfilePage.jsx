c116989821bf7a26ad9f0619ae3ed1bc1a417f51
ChiefsBestPal <67126866+ChiefsBestPal@users.noreply.github.com>
2023-04-11 00:33:07 -0400

    Started edit feature for profile page / user account

--- frontend/src/pages/ProfilePage/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage/ProfilePage.jsx
@@ -4,7 +4,7 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faEnvelope, faPhone,faUser} from "@fortawesome/free-solid-svg-icons";
 import Cookies from 'js-cookie';
 import toTitleCase from '../../utils/strings_and_text_responses'
-import {useParams} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 import CommentAPIService from "../BACKEND_DEBUG/CommentAPIService";
 import UserRESTAPI from "../../restAPI/UserAPI";
 import jwtDecode from "jwt-decode";
@@ -39,10 +39,15 @@ const ProfilePage = () => {
         </div>
     }
     let isViewingOwnProfile;
+    let hasAdminTokenClaims;
     try {
-        isViewingOwnProfile = jwtDecode(Cookies.get("access_token")).user_id === url_params.uid || url_params.uid  === "current"
+        let jwtdecoded = jwtDecode(Cookies.get("access_token"));
+        isViewingOwnProfile = jwtdecoded.user_id === url_params.uid || url_params.uid  === "current"
+        hasAdminTokenClaims = jwtdecoded.hasOwnProperty('admin');
+
     }catch {
         isViewingOwnProfile = false;
+        hasAdminTokenClaims = false;
     }
 
     function checkIfItemExists(item){
@@ -67,28 +72,33 @@ const ProfilePage = () => {
                 </h1>
                 <hr className="profile-line-seperator"></hr>
 
+                <h1  className="profile-content-lastSeen_title">
+                    Last seen:
+                </h1>
                 <h1 className="profile-content-lastSeen">
-                    Last logged in : {new Date(parseInt(userProfileData.lastSeenEpoch))
+                    {new Date(parseInt(userProfileData.lastSeenEpoch))
                     .toLocaleString('us-en',DATETIME_OPTIONS)}
                 </h1>
                 <h1  className="profile-content-dateAdded_title">
-                    Date Added :
+                    Member since:
                 </h1>
                 <h1  className="profile-content-dateAdded">
-                    Member since : {checkIfItemExists(new Date(parseInt(userProfileData.creationEpoch))
+                        {checkIfItemExists(new Date(parseInt(userProfileData.creationEpoch))
                     .toLocaleString('us-en',DATETIME_OPTIONS))}
                 </h1>
                 {
-                    isViewingOwnProfile &&
+                    (isViewingOwnProfile || hasAdminTokenClaims)   &&
                 <footer>
+                    <Link to={{ pathname: `/profile/${url_params.uid}?edit`, state: { userProfileData } }}>
                     <button className="update-profile-btn">Edit my profile</button>
+                        </Link>
                 </footer>
                 }
             </container>
 
             <container className="profile_container_2">
 
-                <h1 className="profile-content-title">Information (Use CHATgpt?)</h1>
+                <h1 className="profile-content-title"> Bio </h1>
                 <h1 className="profile-content-introduction">My name is [insert name] and I am a young aspiring software engineer. From a very young age, I have been fascinated by the world of technology and computers, and I have always been intrigued by how software works. As I grew up, I began exploring different programming languages, experimenting with various coding projects, and expanding my knowledge of computer science.
 
 My passion for software engineering grew stronger as I started to see how it can positively impact people's lives. I believe that software engineering has the potential to solve some of the world's most pressing problems and make a significant difference in society. As a result, I am determined to become a skilled software engineer and contribute to the field in a meaningful way.
