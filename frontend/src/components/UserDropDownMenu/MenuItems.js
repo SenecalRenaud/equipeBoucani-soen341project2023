1db6afe01239273d544442c2ae6318ad4ca5e2aa
ChiefsBestPal <67126866+ChiefsBestPal@users.noreply.github.com>
2023-04-13 18:02:39 -0400

    Fixed and prettified my Dropdown menus

--- frontend/src/components/UserDropDownMenu/MenuItems.js
+++ frontend/src/components/UserDropDownMenu/MenuItems.js
@@ -37,58 +37,57 @@ export default function UserRelationPermsFSM(user1,user2){
   switch (isSelf | ub2 | ub1) {
 
     case 0b00101: //Applicant views other Applicant
-      return <div></div>
+      return <></>
     case 0b10101: //Applicant views itself
-      return <div>
+      return <>
         <li> <Link to="/current/applications"> My Applications </Link> </li>
         <li> <Link to="/current/interviews"> My Interviews </Link> </li>
-    </div>
+    </>
     case 0b01001: //Applicant views Employer
-        //TODO : Might want to encrypt uid here if could be dangerous to leak it / or risk it being hacked
-      return <div>
+      return <>
         <li> <Link to={"/" + user2.uid +  "/jobpostings"}>
           {user2.firstName} {user2.lastName}'s Job Postings </Link> </li>
-      </div>
+      </>
     case 0b01101: //Applicant views Admin
-      return <div></div>
+      return <></>
     case 0b00110: //Employer views Applicant
-      return <div>
+      return <>
       <li>
         <Link to={"/" + user2.uid + "/makeOffer"}> Make {user2.firstName} {user2.lastName} an offer</Link>
       </li>
-      </div>
+      </>
     case 0b01010: //Employer views other Employer
-      return <div>
+      return <>
       <li> <Link to={"/" + user2.uid +  "/jobpostings"}>
           {user2.firstName} {user2.lastName}'s Job Postings </Link> </li>
-      </div>
+      </>
     case 0b11010: //Employer views itself
-      return <div>
+      return <>
         <li> <Link to="/current/jobpostings"> My Job Postings </Link></li>
         <li> <Link to="/current/interviews"> My Interviews </Link></li>
-      </div>
+      </>
     case 0b01110: //Employer views Admin
-      return <div></div>
+      return <></>
     case 0b00111: //Admin views Applicant
-      return <div>
+      return <>
         <li> <Link to={"/" + user2.uid +  "/applications"}> {user2.email}'s Applications </Link> </li>
         <li> <Link to={"/" + user2.uid +  "/interviews"}> {user2.email}'s Interviews </Link> </li>
-      </div>
+      </>
     case 0b01011: //Admin views Employer
-      return <div>
+      return <>
         <li> <Link to={"/" + user2.uid +  "/jobpostings"}> {user2.email}'s Job Postings </Link> </li>
         <li> <Link to={"/" + user2.uid +  "/interviews"}> {user2.email}'s Interviews </Link> </li>
-      </div>
+      </>
     case 0b01111: //Admin views other Admin
-      return <div></div>
+      return <></>
     case 0b11111: //Admin views itself
-      return <div>
+      return <>
       <li> <Link to={"/admin_commandpanel"}> Admin Command Panel </Link></li>
-      </div>
+      </>
     default:
-      return <div>
+      return <>
         <li> <b>Invalid/Unaccessible User ! </b>  </li>
-      </div>
+      </>
 
   }
 
